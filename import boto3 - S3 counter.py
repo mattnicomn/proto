3e128b04ba70020b554c7cb680286700754f933d
Matthew Nico <99774762+mattnicomn@users.noreply.github.com>
2023-08-14 13:38:17 -0400

    Add files via upload

---
+++ import boto3 - S3 counter.py
@@ -0,0 +1,14 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+def lamnda_handler(event, context)
+
+  count = 0
+  response =s3.list_objects_v2(bucket=event['bucket'])
+
+  if 'Contents' in response:
+    for object in response['Contents']:
+      if object['Key'][-1] != '/':
+        count += 1
+  return("Number of files: ", count)
