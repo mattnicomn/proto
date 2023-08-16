1843e993e7d9c1fda66f6ab57ef9f5d60cb12384
Matthew Nico <99774762+mattnicomn@users.noreply.github.com>
2023-08-16 09:48:05 -0400

    Update import boto3 - S3 counter.py

--- import boto3 - S3 counter.py
+++ import boto3 - S3 counter.py
@@ -6,11 +6,19 @@ def lamnda_handler(event, context)
 
   count = 0
   response =s3.list_objects_v2(bucket=event['bucket'])
-
+  #paginator = s3.get_paginator('list_objects_v2')
+  #iterator = paginator.paginate(Bucket=event['bucket']
+  
   if 'Contents' in response:
     for object in response['Contents']:
       if object['Key'][-1] != '/':
         count += 1
   return("Number of files: ", count)
 
+
+  #if 'Contents' in response:
+    #for object in iterator:
+      #if object['Key'].endswith('/')
+        #count += 1
+  #return (f'Number of objects in Bucket: {bucket.name} Key : {file.key}')
 #https://stackoverflow.com/questions/42673764/boto3-s3-get-files-without-getting-folders
